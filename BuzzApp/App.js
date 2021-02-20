860e9b080d7c02d6b173d799c21ae7a1c9c2bdfc
IKIK20 <67692637+IKIK20@users.noreply.github.com>
2021-02-20 12:38:02 +0530

    Add files via upload

---
+++ BuzzApp/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import {Fb} from './screens/Fb'
+import {In} from './screens/In'
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Facebook: { screen: Fb },
+  Instagram: { screen: In }
+});
+
+const AppContainer = createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
